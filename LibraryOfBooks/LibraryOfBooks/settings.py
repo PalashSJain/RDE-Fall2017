83fbc7b24859726c7e22a1f7d7d1ab0076df4d0d
PalashSJain <pxj9579@rit.edu>
2017-11-18 23:50:46 -0800

    Added HelloWorld view and template

--- LibraryOfBooks/LibraryOfBooks/settings.py
+++ LibraryOfBooks/LibraryOfBooks/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'LibraryOfBooks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['enduser/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

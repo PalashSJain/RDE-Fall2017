68b67d53b4817695dce2608df2135257c35448e1
PalashSJain <pxj9579@rit.edu>
2017-11-19 10:36:39 -0800

    Moved templates outside of enduser app

--- LibraryOfBooks/LibraryOfBooks/settings.py
+++ LibraryOfBooks/LibraryOfBooks/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'LibraryOfBooks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['enduser/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

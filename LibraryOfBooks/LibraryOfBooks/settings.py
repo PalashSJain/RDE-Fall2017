457f7053c8938aa10abc9d9ef01d266bbe3e8b3f
PalashSJain <pxj9579@rit.edu>
2017-11-25 14:39:18 -0800

    Set DEBUG = False. Production mode Turned on.

--- LibraryOfBooks/LibraryOfBooks/settings.py
+++ LibraryOfBooks/LibraryOfBooks/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nz3c9+f+!4tae)+70)tl+=3=98k57c19vids4m^pm-nl!=vzu*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
